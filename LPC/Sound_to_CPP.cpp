c03480e55db046400692493dd26529c979514057
davidweenink <david.weenink@uva.nl>
2026-08-14 20:00:36 +0200

    Sound_to_CPP.h && Sound_to_CPP.cpp

---
+++ LPC/Sound_to_CPP.cpp
@@ -0,0 +1,42 @@
+/* Sound_to_CPP.cpp
+ *
+ * Copyright (C) 2026 David Weenink
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Sound_to_CPP.h"
+
+autoCPP Sound_to_CPP (constSound me, double pitchFloor, double pitchCeiling, double dt, double maximumFrequency, 
+	double preEmphasisFrequency,
+	bool subtractTrendBeforeSmoothing, double timeAveragingWindow, double quefrencyAveragingWindow,
+	double deltaF0, kVector_peakInterpolation peakInterpolationType,
+	double qstartFit, double qendFit, kCepstrum_trendType lineType, kCepstrum_trendFit fitMethod
+) {
+	try {
+		autoPowerCepstrogram ps = Sound_to_PowerCepstrogram (me, pitchFloor, dt, maximumFrequency, preEmphasisFrequency);
+		if (subtractTrendBeforeSmoothing) {
+			ps = PowerCepstrogram_subtractTrend (ps.get(), qstartFit, qendFit, lineType, fitMethod).move();
+		}
+		autoPowerCepstrogram smoothed = PowerCepstrogram_smooth (ps.get(), timeAveragingWindow, quefrencyAveragingWindow);
+		autoCPP cpp = PowerCepstrogram_to_CPP (smoothed.get(), pitchFloor, pitchCeiling, deltaF0,
+			peakInterpolationType,  qstartFit,  qendFit, lineType, fitMethod);
+		return cpp;
+	} catch (MelderError) {
+		Melder_throw (me, U": canot create CPP.");
+	}
+}
+
+
+/* End of file Sound_to_CPP.cpp */
