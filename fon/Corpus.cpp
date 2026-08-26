bc47fda674037702c6165926869f2f5da4626c58
PaulBoersma <paul.boersma@uva.nl>
2026-08-26 10:21:16 +0200

    Corpus: extract Sound, components, education regions, speakers Table

--- fon/Corpus.cpp
+++ fon/Corpus.cpp
@@ -160,4 +160,108 @@ autoTextGrid Corpus_extractTextGrid_number (Corpus me, integer recordingNumber) 
 	return thee;
 }
 
+autoSound Corpus_extractSound_number (Corpus me, integer recordingNumber) {
+	Melder_require (recordingNumber >= 1,
+		U"The recording number (", recordingNumber, U") should be at least 1.");
+	Melder_require (recordingNumber <= my recordings -> rows.size,
+		U"The recording number (", recordingNumber, U") should be at most the number of recordings (", my recordings -> rows.size, U").");
+	structMelderFolder wavFolder { };
+	Melder_relativePathToFolder (my folderWithSoundFiles.get(), & wavFolder);
+	structMelderFolder compFolder { };
+	MelderFolder_getSubfolder (& wavFolder, Table_getStringValue_a (my recordings.get(), recordingNumber, 1), & compFolder);
+	structMelderFolder countryFolder { };
+	MelderFolder_getSubfolder (& compFolder, Table_getStringValue_a (my recordings.get(), recordingNumber, 2), & countryFolder);
+	structMelderFile soundFile { };
+	MelderFolder_getFile (& countryFolder, Table_getStringValue_a (my recordings.get(), recordingNumber, 3), & soundFile);
+	autoSound thee = Sound_readFromSoundFile (& soundFile);
+	Thing_setName (thee.get(), Table_getStringValue_a (my recordings.get(), recordingNumber, 3));
+	return thee;
+}
+
+autoCorpus Corpus_extractComponents (Corpus me,
+	bool comp_a, bool comp_b, bool comp_c, bool comp_d, bool comp_e,
+	bool comp_f, bool comp_g, bool comp_h, bool comp_i, bool comp_j,
+	bool comp_k, bool comp_l, bool comp_m, bool comp_n, bool comp_o
+) {
+	bool components [15] {
+		comp_a, comp_b, comp_c, comp_d, comp_e,
+		comp_f, comp_g, comp_h, comp_i, comp_j,
+		comp_k, comp_l, comp_m, comp_n, comp_o
+	};
+	autoCorpus thee = Data_copy (me);
+	for (integer row = thy recordings -> rows.size; row >= 1; row --) {
+		conststring32 component = Table_getStringValue_a (thy recordings.get(), row, 1);
+		Melder_require (Melder_stringMatchesCriterion (component, kMelder_string::STARTS_WITH, U"comp-", true),
+			U"Component ", component, U" does't start with “comp-”.");
+		const char32 componentLetter = component [5];
+		Melder_require (componentLetter >= U'a' && componentLetter <= U'o',
+			U"The sixth character of component ", component, U" should have been between “a” and “o”.");
+		if (! components [componentLetter - U'a']) {
+			Table_removeRow (thy recordings.get(), row);
+			thy textGrids. removeItem (row);
+		}
+	}
+	return thee;
+}
+
+autoCorpus Corpus_extractEducationRegions (Corpus me,
+	bool sHolland, bool nHolland, bool wUtrecht, bool zeeland, bool eUtrecht,
+	bool guelders, bool veluwe, bool wFriesland, bool polders, bool achterhoek,
+	bool overijssel, bool drenthe, bool groningen, bool frisia, bool nBrabant,
+	bool eLimburg, bool netherlands, bool sBrabant, bool eFlanders, bool wFlanders,
+	bool wLimburg, bool nBelgium, bool sBelgium, bool reg_other, bool reg_unknown
+) {
+	bool regions [25] {
+		sHolland, nHolland, wUtrecht, zeeland, eUtrecht,
+		guelders, veluwe, wFriesland, polders, achterhoek,
+		overijssel, drenthe, groningen, frisia, nBrabant,
+		eLimburg, netherlands, sBrabant, eFlanders, wFlanders,
+		wLimburg, nBelgium, sBelgium, reg_other, reg_unknown
+	};
+	autoCorpus thee = Data_copy (me);
+	for (integer row = thy recordings -> rows.size; row >= 1; row --) {
+		TextGrid textgrid = thy textGrids.at [row];
+		bool foundMatch = false;
+		for (integer itier = 1; itier <= textgrid -> tiers->size; itier ++) {
+			IntervalTier tier = (IntervalTier) textgrid -> tiers->at [itier];   // TODO: check cast
+			conststring32 tierName = tier -> name.get();
+			if (Melder_stringMatchesCriterion (tierName, kMelder_string::STARTS_WITH, U"ort/", true)) {
+				const conststring32 speakerName = & tier -> name [4];
+				integer speakerRow = Table_searchColumn (thy speakers.get(), 5, speakerName);
+				if (speakerRow == 0) {
+					if (! str32equ (speakerName, U"BACKGROUND") &&
+						! str32equ (speakerName, U"COMMENT") &&
+						! str32equ (speakerName, U"UNKNOWN") &&
+						! str32equ (speakerName, U"N00383"))
+						Melder_crash (U"<<", speakerName, U">> <<", tierName, U">>");
+				} else {
+					conststring32 speakerRegionCode = Table_getStringValue_a (thy speakers.get(), speakerRow, 17);
+					integer icode = 0;
+					for (; icode < 25; icode ++)
+						if (str32equ (speakerRegionCode, Corpus_CGN_regionCodes [icode]))
+							break;
+					if (icode >= 25) {
+						if (str32equ (speakerRegionCode, U"regB"))
+							icode = 24;
+					}
+					if (icode >= 25)
+						Melder_crash (icode, U" <<", speakerRegionCode, U">>");
+					if (regions [icode]) {
+						foundMatch = true;
+						break;
+					}
+				}
+			}
+		}
+		if (! foundMatch) {
+			/*
+				Remove this TextGrid and Sound.
+			*/
+			thy textGrids. removeItem (row);
+			Table_removeRow (thy recordings.get(), row);
+		}
+	}
+	return thee;
+}
+
 /* End of file Corpus.cpp */
